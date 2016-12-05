7404b364c00b3116426892f53742d16a9e6c3fbc
Marissa <marissaokada@gmail.com>
2016-12-05 15:27:08 -0800

    Add additional functions to Game class

--- game.js
+++ game.js
@@ -15,38 +15,36 @@ var reader = readline.createInterface({
   output: process.stdout
 });
 
-Game.prototype.move = function(row, col) {
-  console.log('in move', row, col);
+Game.prototype.mark = function(row, col) {
   this.board[row - 1][col - 1] = 'X';
-  console.log('board', this.board);
 };
+
+Game.prototype.detectWin = function () {
+  // TODO
+};
+
+Game.prototype.nextMove = function () {
+  // this.detectWin();
+};
+
+// need detection function to go with nextMove in order to detect a win
 
 Game.prototype.promptUser = function() {
   var row,
       col,
       context = this;
-  reader.question('Place your mark as: row, column?',
+  reader.question('Place your mark as: row, column',
     function(userInput) {
       userInput = userInput.split(',');
       row = userInput[0];
       col = userInput[1];
-      context.move(row, col);
+      context.mark(row, col);
     });
-}
+};
 
 Game.prototype.play = function() {
   this.promptUser();
 };
-
-// Game.prototype.turn = function() {
-//   if (this.player1 === true) {
-//     this.player1 = !this.player1;
-//     this.player2 = !this.player2;
-//   } else if (this.player2 === true) {
-//     this.player2 = !this.player2;
-//     this.player1 = !this.player1;
-//   }
-// };
 
 var tictac = new Game();
 tictac.play();
