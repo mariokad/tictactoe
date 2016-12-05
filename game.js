4c18e1f4c8cc9ab0ffdf2bc67958f8a7c1006d42
Marissa <marissaokada@gmail.com>
2016-12-05 15:02:01 -0800

    Add in prompt for user to choose position

--- game.js
+++ game.js
@@ -1,10 +1,40 @@
 var Game = function() {
-  this.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-  this.turn = false;
+  this.board = [
+  ['', '', ''], 
+  ['', '', ''], 
+  ['', '', '']
+  ];
+  this.player1 = true;
+  this.player2 = false;
 };
 
-Game.prototype.start = function() {
-  return this.board;
+var readline = require("readline");
+
+var reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+Game.prototype.promptUser = function() {
+  reader.question('')
+}
+
+Game.prototype.play = function() {
+  Game.promptUser();
+};
+
+// Game.prototype.turn = function() {
+//   if (this.player1 === true) {
+//     this.player1 = !this.player1;
+//     this.player2 = !this.player2;
+//   } else if (this.player2 === true) {
+//     this.player2 = !this.player2;
+//     this.player1 = !this.player1;
+//   }
+// };
+
+Game.prototype.move = function(row, col) {
+
 };
 
 var tictac = new Game();
